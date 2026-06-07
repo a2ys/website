22ba9a40c7bc7de86c50c6c64d2a0eb94edd3af9
Aayush Shukla <oreus.aks@gmail.com>
2026-06-08 01:14:31 +0530

    fix: incorrect import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Nav from "@/components/Nav";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 import Footer from "@/components/Footer";
-import Metadata from "next";
+import { Metadata } from "next";
 
 const anonymousPro = Anonymous_Pro({
   subsets: ["latin"],
